489a019beae348664bfd63cb03500641e2c25e6f
washics18 <washics18@hotmail.com>
2021-05-12 16:08:42 -0300

    Testado Rest e Banco H2

---
+++ src/main/java/com/washics18/ProjetoVendas/ProjetoVendasApplication.java
@@ -0,0 +1,13 @@
+package com.washics18.ProjetoVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/washics18/ProjetoVendas/ProjetoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.washics18.ProjetoVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
