8a7115ad9f4d441ccbc92e2eb2032a140acc2cc3
washics18 <washics18@hotmail.com>
2021-05-14 14:28:54 -0300

    Criando operação de instaciação

--- src/main/java/com/washics18/ProjetoVendas/ProjetoVendasApplication.java
+++ src/main/java/com/washics18/ProjetoVendas/ProjetoVendasApplication.java
@@ -1,13 +1,36 @@
 package com.washics18.ProjetoVendas;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.washics18.ProjetoVendas.domain.Categoria;
+import com.washics18.ProjetoVendas.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetoVendasApplication {
+public class ProjetoVendasApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoVendasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+		
+		
 	}
 
 }
